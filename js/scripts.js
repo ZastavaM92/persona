b8e13e0ce8baa10dc415082fc542978882db4b3d
ZastavaM92 <graup45@gmail.com>
2022-05-24 13:38:04 +0500

    ЖЕСТЬ ДОБАВИЛИ ЗВУК

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,48 @@
 let stalkerMoment = {       //stalker
-    names:[
+    names: [
         'Толя',
         'Стас',
         'Артём',
         'Глеб',
         'Григорой',
         'Вася',
+        'Эдик',
+        'Женя',
+        'Ваня',
+        'Саня',
+        'Рома',
     ],
-    klichka:[               //klichka (fam)
+    klichka: [               //klichka (fam)
         'Кабан',
         'Пуля',
         'Обрез',
         'Баян',
         'Мышь',
         'Паращютист',
+        'Летчик',
+        'Свинец',
+        'Тугой',
+        'Свисток',
+        'Щелбан',
     ]
 }
 
 let groupsMoment = {
-    group:[
+    group: [
         "Одиночки",
         "Бандиты",
         "Чистое небо",
         "Долг",
+        "Свобода",
+        "Наемники",
+        "Ученые",
+        "Военные",
+        "Монолит",
     ]
 }
 
 let rangsMoment = {
-    rangi:[
+    rangi: [
         "Отмычка",
         "Новичок",
         "Опытный",
@@ -35,6 +50,9 @@ let rangsMoment = {
         "Бывалый",
         "Ветеран",
         "Легенда Зоны",
+        "Сын Зоны",
+        "Дитя Зоны",
+        "Черная Зона",
     ]
 }
 
@@ -51,7 +69,13 @@ let skil = {     //skil (sposobnosti)
         "АКС-74у 'Гексагон', АПС, Комбинезон 'СКАТ-9М'",
         "АКМС 'Бубен', ПЯ 'Грач', Комбинезон ЧН-3б",
         "L-85, Desert Eagle, Экзоскелет 'Долга'",
-    ]}
+        "Colt M4A1 'ACOG', Glock 17, Комбинезон 'Ветер Свободы'",
+        "ВСС 'Кобра', Обрез БМ-16, ПС3-9д 'Броня Долга'",
+        "Вепрь-12, Glock 17, ПС5-М 'Универсальная защита'",
+        "FN SCAR-H 'Eotech', HK USP 'Match', Комбинезон 'Монолита'",
+        "ПКП 'Печенег', HK USP 'Match', Экзоскелет 'Монолита'",
+    ]
+}
 
 let icon = {
     photo: [
@@ -65,8 +89,13 @@ let icon = {
         '<img src="css/img/8.png" width="200px" height="200px">',
         '<img src="css/img/9.png" width="200px" height="200px">',
         '<img src="css/img/10.png" width="200px" height="200px">',
+        '<img src="css/img/11.png" width="200px" height="200px">',
+        '<img src="css/img/12.png" width="200px" height="200px">',
+        '<img src="css/img/13.png" width="200px" height="200px">',
+        '<img src="css/img/14.png" width="200px" height="200px">',
     ]
 }
+
 
 let knopka = document.querySelector('#knopka')
 let kart = document.querySelector('.kart')
@@ -76,6 +105,8 @@ let txt = document.querySelector('.txt')
 knopka.addEventListener('click', function () {
     kart.innerHTML = ''
     txt.innerHTML = ''
+    var audio = new Audio('js/click.mp3')
+    audio.autoplay = true
 
     let randIndexNames = Math.floor(Math.random() * (stalkerMoment.names.length))
     let randicon = Math.floor(Math.random() * icon.photo.length)
@@ -85,7 +116,7 @@ knopka.addEventListener('click', function () {
     let randitem = Math.floor(Math.random() * (skil.item.length))
     kart.innerHTML = icon.photo[randicon]
     txt.innerHTML = 'Имя и Кличка: ' + stalkerMoment.names[randIndexklichka] + ' ' + stalkerMoment.klichka[randIndexNames] + '<br>' + 'Группировка: ' + groupsMoment.group[randgroups] + '<br>' + 'Ранг: ' + rangsMoment.rangi[randrangs] + '<br>' + 'Инвентарь: ' + skil.item[randitem]
-    })
+})
 
 
 
